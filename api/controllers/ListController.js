0f1be8750b65569416870bf87ca20850d3760ab7
Ricardo Romo Ramírez <ricardo.romo.ramirez@gmail.com>
2015-12-05 22:07:49 -0600

    Fixed problem with async creating songs in a list

--- api/controllers/ListController.js
+++ api/controllers/ListController.js
@@ -16,54 +16,63 @@ module.exports = {
     Model
       .create(values)
       .then(function(list){
-        var songs = req.param('songs',[]);;
+        var songs = req.param('songs',[]);
+        var songToSave = 0;
         for(var i in songs){
           var song = songs[i];
           var songId = song.id;
-          if(songId){
-            //if an id is passed as the song, then try to find it and add it to the list
-            Song.findOne({id: songId, adminId:values.adminId})
+          Song.findOne({id: songId, adminId:values.adminId})
             .then(function(err, thisSong) {
               if(!err && thisSong){
-                list.songs.add(thisSong.id);
+                return thisSong.id;
               }else{
-                sails.log.error("The id: " + songId +
-                                " doesn't exist for the admin: " + values.adminId);
-              }
-            });
-          }else{
-            //if no id passed, then try to get the title, artist and genre and create a new song
-            var title = song.title;
-            var artist = song.artist;
-            var  genre = song.genre;
-
-            if(title && artist && values.adminId){
-              Song.findOne({title: title, artist:artist, adminId:values.adminId})
-              .exec(function(err, thisSong){
-                if(!err && thisSong){
-                  list.songs.add(thisSong.id);
-                }else{
-                  Song.create({title:title
-                          , artist:artist
-                          , adminId:values.adminId
-                          , genre: genre})
-                  .exec(function createCB(err, thisSong){
-                    if(!err){
-                      list.songs.add(thisSong.id);
-                    }else{
-                      sails.log.error("Error saving a song: " + err);
-                    }
-                  });
+                song = songs[songToSave];
+                if(song.title && song.artist && values.adminId){
+                  return Song.findOne({
+                                title: song.title
+                                , artist:song.artist
+                                , adminId:values.adminId
+                              });
                 }
-              });
-            }else{
-              //if there are not admin id or title or artist for the song
-              sails.log.error("There are a missing parameter for a new song - artist:" +
-                                artist + " title: " + title);
-            }
-          }
+                songToSave++;
+                throw new Error("The're are missing params for the song");
+              }
+            })
+            .then(function(songFound){
+              if(!songFound){
+                //if the song was not found, then try to create it
+                song = songs[songToSave++];
+                return Song.create({title:song.title
+                          , artist:song.artist
+                          , adminId:values.adminId
+                          , genre: song.genre});
+              }else if(songFound.id){
+                //if found a song with the same title, artist and from the admin
+                //add it to the list
+                list.songs.add(songFound.id);
+                songToSave++;
+                return null;
+              }else{
+                //if the song was found by the id then add it to the list
+                list.songs.add(songFound);
+                songToSave++;
+                return null;
+              }
+            })
+            .then(function(newSong){
+              if(newSong){
+                list.songs.add(newSong.id);
+                sails.log.info('song created correctly:');
+                sails.log.info(newSong);
+              }
+              if (songToSave === songs.length)
+                list.save();
+            })
+            .catch(function(error){
+              console.log(error);
+            });
+
         }
-        list.save();
         res.created(list);
       })
       .catch(res.serverError);
